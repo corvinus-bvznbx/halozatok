4a8ca3d8af92a7b90cf6c2ed8db550d386f5ef4b
student <balazs.ofra@stud.uni-corvinus.hu>
2021-03-18 15:17:33 +0100

    gyak6

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,27 @@
+﻿window.onload = () => {
+    console.log("betöltődött");
+    var faktoriális = function (n) {
+        let er = 1;
+        for (let i = 2; i <= n; i++) {
+            er = er * i;
+        }
+        return er;
+    }
+    let hova = document.getElementById("ide");
+    for (var s = 0; s < 10; s++) {
+
+        let sor = document.createElement("div");
+        sor.classList.add("sor");
+        hova.appendChild(sor);
+
+        for (var o = 0; o < 10; o++) {
+            let szam = document.createElement("div");
+            szam.classList.add("doboz");
+            szam.classList.add("pascal");
+            szam.style.background = `rgb(${255 / 10 * s},0,0)`;
+            sor.appendChild(szam)
+            szam.innerText = (s+1)*(o+1)
+        }
+
+    }
+}
---
+++ HajosTeszt/HajosTeszt/wwwroot/szorzo.js
@@ -0,0 +1,19 @@
+﻿window.onload = () => {
+    console.warn(":)");
+let hova = document.getElementById("ide");
+for (var s = 0; s < 10; s++) {
+
+    let sor = document.createElement("div");
+    sor.classList.add("sor");
+    hova.appendChild(sor);
+
+    for (var o = 0; o < 10; o++) {
+        let szam = document.createElement("div");
+        szam.classList.add("doboz");
+        szam.style.background = `rgb(${255 / 10 * s},0,0)`;
+        sor.appendChild(szam)
+        szam.innerText = (s + 1) * (o + 1)
+    }
+
+    }
+}
